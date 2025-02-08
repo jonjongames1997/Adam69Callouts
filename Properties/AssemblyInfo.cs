144c8b6dc190ac5d384eecaf517d625fb948dcfe
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-02-08 00:10:27 -0500

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c330ec03-8bb7-467b-a80a-544d0c7839de")]
-[assembly: AssemblyVersion("0.2.5.1")]
-[assembly: AssemblyFileVersion("0.2.5.1")]
+[assembly: AssemblyVersion("0.2.7.0")]
+[assembly: AssemblyFileVersion("0.2.7.0")]
