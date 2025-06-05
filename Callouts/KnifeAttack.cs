0ccfcd998f6f69e6af7e8820f0ae5d01524002c2
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-06-05 12:08:28 -0400

    Update KnifeAttack.cs

--- Callouts/KnifeAttack.cs
+++ Callouts/KnifeAttack.cs
@@ -63,6 +63,8 @@ namespace Adam69Callouts.Callouts
             victim.IsValid();
 
             NativeFunction.Natives.APPLY_PED_DAMAGE_PACK(victim, "HitByVehicle", 1f, 1f);
+            NativeFunction.Natives.IS_PED_INJURED(victim);
+            NativeFunction.Natives.IS_PED_HURT(victim);
 
             suspectBlip = suspect.AttachBlip();
             suspectBlip.Color = System.Drawing.Color.Red;
