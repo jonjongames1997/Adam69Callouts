df1ba429ad125fcaf80851a0cc4e140b15c37bfb
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-07-08 15:02:54 -0400

    Create Globals.cs

---
+++ Utilities/Globals.cs
@@ -0,0 +1,10 @@
+﻿namespace Adam69Callouts.Utilities
+{
+    internal static class Globals
+    {
+        internal static class theApplication
+        {
+            public static bool debugLogging { get; set; }
+        }
+    }
+}
