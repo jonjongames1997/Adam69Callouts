82db6d4b4a0d66d180e13813eef97e1d105f2630
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-07-28 11:56:07 -0400

    Fix blueline dispatch logic and remove unused variable
    Corrects the conditional to use Settings.DisableBluelineDispatch and swaps scanner audio calls for proper integration. Also removes unused 'missionMessages' variable assignments in else branches.

--- Callouts/VehicleBlockingCrosswalk.cs
+++ Callouts/VehicleBlockingCrosswalk.cs
@@ -28,13 +28,13 @@ namespace Adam69Callouts.Callouts
             };
             spawnpoint = LocationChooser.ChooseNearestLocation(list);
             ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
-            if (Settings.BluelineDispatchIntegration == true)
+            if (Settings.DisableBluelineDispatch == true)
             {
-                LSPD_First_Response.Mod.API.Functions.PlayScannerAudioUsingPosition("CRIME_ROAD_BLOCKADE_01", spawnpoint);
+                LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("Adam69Callouts_VehicleBlockingCrosswalk_Audio");
             }
             else
             {
-                LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("Adam69Callouts_VehicleBlockingCrosswalk_Audio");
+                LSPD_First_Response.Mod.API.Functions.PlayScannerAudioUsingPosition("CRIME_ROAD_BLOCKADE_01", spawnpoint);
             }
             CalloutInterfaceAPI.Functions.SendMessage(this, "A vehicle blocking crosswalk");
             CalloutMessage = "Multiple reports of a vehicle blocking crosswalk";
@@ -98,7 +98,6 @@ namespace Adam69Callouts.Callouts
                 }
                 else
                 {
-                    missionMessages = false;
                     return;
                 }
 
@@ -127,7 +126,6 @@ namespace Adam69Callouts.Callouts
             }
             else
             {
-                missionMessages = false;
                 return;
             }
 
