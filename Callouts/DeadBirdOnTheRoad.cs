6d8cdd27b122c8424eefdc08b7cbc3a50f44f7ed
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-07-28 11:44:46 -0400

    Update DeadBirdOnTheRoad.cs

--- Callouts/DeadBirdOnTheRoad.cs
+++ Callouts/DeadBirdOnTheRoad.cs
@@ -21,13 +21,13 @@ namespace Adam69Callouts.Callouts
             };
             spawnpoint = LocationChooser.ChooseNearestLocation(list);
             ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
-            if (Settings.BluelineDispatchIntegration == true)
+            if (Settings.DisableBluelineDispatch == true)
             {
-                LSPD_First_Response.Mod.API.Functions.PlayScannerAudioUsingPosition("CRIME_ANIMAL_KILLED_02", spawnpoint);
+                LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("Adam69Callouts_DeadBirdOnTheRoad_Audio_01");
             }
             else
             {
-                LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("Adam69Callouts_DeadBirdOnTheRoad_Audio_01");
+                LSPD_First_Response.Mod.API.Functions.PlayScannerAudioUsingPosition("CRIME_ANIMAL_KILLED_02", spawnpoint);
             }
             CalloutInterfaceAPI.Functions.SendMessage(this, "A dead bird is on the road.");
             CalloutMessage = "Dead Bird Reported";
