7f29298becb931780b2372e1bd98073b914ae616
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-07-29 17:05:28 -0400

    Update BicyclePursuit.cs

--- Callouts/BicyclePursuit.cs
+++ Callouts/BicyclePursuit.cs
@@ -19,7 +19,7 @@ namespace Adam69Callouts.Callouts
             spawnpoint = World.GetNextPositionOnStreet(MainPlayer.Position.Around(1000f));
             ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
             CalloutInterfaceAPI.Functions.SendMessage(this, "A civilian is evading arrest");
-            if (Settings.DisableBluelineDispatch == true)
+            if (Settings.BluelineDispatch == true)
             {
                 LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("Adam69Callouts_Bicycle_Pursuit_Audio");
             }
