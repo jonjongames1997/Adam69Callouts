22c22783336c4d229754aa886a1f6cc9b4ac0fea
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-07-29 17:05:44 -0400

    Update Loitering.cs

--- Callouts/Loitering.cs
+++ Callouts/Loitering.cs
@@ -18,7 +18,7 @@ namespace Adam69Callouts.Callouts
             {
                 spawnpoint = World.GetNextPositionOnStreet(MainPlayer.Position.Around(100f));
                 ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
-                if (DisableBluelineDispatch == true)
+                if (BluelineDispatch == true)
                 {
                     LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("Adam69Callouts_Loitering_Audio");
                 }
