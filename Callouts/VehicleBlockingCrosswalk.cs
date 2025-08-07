61560954f1969bd6846d5ad07429c8abf315be80
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-08-07 02:14:22 -0400

    Simplify boolean check in callout initialization
    Removed redundant comparison to 'true' in the Settings.BluelineDispatch conditional, improving code clarity in VehicleBlockingCrosswalk callout.

--- Callouts/VehicleBlockingCrosswalk.cs
+++ Callouts/VehicleBlockingCrosswalk.cs
@@ -28,7 +28,7 @@ namespace Adam69Callouts.Callouts
             };
             spawnpoint = LocationChooser.ChooseNearestLocation(list);
             ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
-            if (Settings.BluelineDispatch == true)
+            if (Settings.BluelineDispatch)
             {
                 LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("Adam69Callouts_VehicleBlockingCrosswalk_Audio");
             }
