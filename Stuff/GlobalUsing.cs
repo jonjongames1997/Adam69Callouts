4621a9459d1f9f0d8e9e8f18f2cf9e4eec82e1b6
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-09-11 00:44:17 -0400

    Reorganize and clean up global using directives
    Removed unused and redundant global using statements in GlobalUsing.cs, and reordered the remaining directives for improved clarity and maintainability.

--- Stuff/GlobalUsing.cs
+++ Stuff/GlobalUsing.cs
@@ -1,34 +1,25 @@
 ﻿
 #region 
 
+global using Adam69Callouts.Callouts;
+global using Adam69Callouts.Stuff;
+global using Adam69Callouts.Utilities;
+global using LSPD_First_Response.Mod.API;
+global using LSPD_First_Response.Mod.Callouts;
+global using Rage;
+global using Rage.Native;
+global using RAGENativeUI.Elements;
 global using System;
 global using System.Collections.Generic;
 global using System.Drawing;
-global using LSPD_First_Response.Mod.API;
-global using LSPD_First_Response.Mod.Callouts;
-global using Adam69Callouts.Stuff;
+global using System.IO;
 global using System.Linq;
+global using System.Reflection;
 global using System.Text;
 global using System.Threading.Tasks;
-global using System.Net;
-global using Rage;
-global using Rage.Native;
-global using Adam69Callouts.Callouts;
-global using static Adam69Callouts.IniFile;
-global using System.Threading;
-global using static Adam69Callouts.Settings;
-global using System.IO;
-global using static Adam69Callouts.Stuff.Helper;
-global using RAGENativeUI.Elements;
-global using RAGENativeUI;
-global using static Adam69Callouts.Utilities.LoggingManager;
 global using System.Windows.Forms;
-global using System.Reflection;
-global using System.Xml;
-global using Adam69Callouts.Utilities;
-global using Adam69Callouts.VersionChecker;
-global using static Adam69Callouts.VersionChecker.PluginCheck;
-global using LSPD_First_Response.Mod.Utils;
+global using static Adam69Callouts.Stuff.Helper;
+global using static Adam69Callouts.Utilities.LoggingManager;
 
 
 #endregion
