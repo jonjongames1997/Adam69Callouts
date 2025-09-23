62d4442a6201412b14a4ef97557cabd57f0c306d
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-09-23 17:55:44 -0400

    Update OfficerDown.cs

--- Callouts/OfficerDown.cs
+++ Callouts/OfficerDown.cs
@@ -131,9 +131,17 @@ namespace Adam69Callouts.Callouts
         {
             if (MainPlayer.DistanceTo(officer) <= 10f)
             {
-                Game.DisplayHelp("Press ~y~" + Settings.Dialog.ToString() + "~w~ to Call in a officer down to ~b~dispatch~w~.", 5000);
+                if (Settings.HelpMessages)
+                {
+                    Game.DisplayHelp("Press ~y~" + Settings.Dialog.ToString() + "~w~ to Call in a officer down to ~b~dispatch~w~.");
+                }
+                else
+                {
+                    Settings.HelpMessages = false;
+                    return;
+                }
 
-                if (Game.IsKeyDown(Settings.Dialog))
+                if (Game.IsKeyDown(System.Windows.Forms.Keys.Y))
                 {
                     counter++;
 
