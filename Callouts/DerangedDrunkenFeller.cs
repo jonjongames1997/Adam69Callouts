d7a0e61e0eaf18e09c387adeba3e397aa94d0c48
jonjongames1997 <homedawg055@gmail.com>
2026-08-03 17:49:55 -0400

    Add branching outcomes to drunken feller callout
    Refactors `DerangedDrunkenFeller` to use per-instance state and introduces randomized scenario flow with four encounter types: aggressive, compliant, fleeing, and passing out. The dialogue/action sequence is now routed through dedicated handlers, with updated suspect behavior, animations, blip state, and weapon selection logic to make the callout less linear and more varied.

--- Callouts/DerangedDrunkenFeller.cs
+++ Callouts/DerangedDrunkenFeller.cs
@@ -1,21 +1,30 @@
 ﻿using CalloutInterfaceAPI;
 using Adam69Callouts.Common;
+using RAGENativeUI;
+using Rage;
 
 namespace Adam69Callouts.Callouts
 {
-
     [CalloutInterface("[Adam69 Callouts] - Deranged Drunken Feller", CalloutProbability.Medium, "Reports of a drunken feller", "Code 2", "LSPD")]
-
-
     public class DerangedDrunkenFeller : Callout
     {
-        private static Ped suspect;
-        private static Vector3 spawnpoint;
-        private static Blip blip;
-        private static int counter;
-        private static string malefemale;
-        private static int copgender;
+        private Ped suspect;
+        private Vector3 spawnpoint;
+        private Blip blip;
+        private int counter;
+        private string malefemale;
         private static readonly string[] wepList = new string[] { "weapon_pistol", "weapon_combatmg", "weapon_combatpistol" };
+        private static readonly Random random = new Random();
+
+        private enum DrunkScenario
+        {
+            Aggressive,
+            Compliant,
+            Flees,
+            PassesOut
+        }
+
+        private DrunkScenario scenario;
 
         public override bool OnBeforeCalloutDisplayed()
         {
@@ -37,8 +46,17 @@ namespace Adam69Callouts.Callouts
             }
 
             Game.DisplayNotification("web_adam69callouts", "web_adam69callouts", "~w~Adam69 Callouts", "Deranged Drunken Feller", "~b~Dispatch~w~: Suspect has been located. Respond ~r~Code 2~w~.");
-
             LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("Adam69Callouts_Respond_Code_2_Audio");
+
+            scenario = (DrunkScenario)random.Next(Enum.GetValues(typeof(DrunkScenario)).Length);
+
+            suspect = new Ped(spawnpoint)
+            {
+                IsPersistent = true,
+                BlockPermanentEvents = true
+            };
+
+            suspect.Tasks.PlayAnimation(new AnimationDictionary("random@drunk_driver_1"), "drunk_driver_stand_loop_dd2", -1f, AnimationFlags.Loop);
 
             blip = suspect.AttachBlip();
             blip.Color = System.Drawing.Color.Red;
@@ -66,40 +84,12 @@ namespace Adam69Callouts.Callouts
         {
             if (MainPlayer.DistanceTo(suspect) <= 10f)
             {
-
                 if (Game.IsKeyDown(System.Windows.Forms.Keys.Y))
                 {
                     counter++;
                     try
                     {
-                        if (counter == 1)
-                        {
-                            NativeFunction.Natives.TASK_TURN_PED_TO_FACE_ENTITY(suspect, MainPlayer, -1);
-                            Game.DisplaySubtitle("~b~You~w~: What goin' on, feller? Have anything to drink today?");
-                        }
-                        if (counter == 2)
-                        {
-                            suspect.Tasks.PlayAnimation(new AnimationDictionary("random@drunk_driver_1"), "drunk_driver_stand_loop_dd2", -1f, AnimationFlags.Loop);
-                            Game.DisplaySubtitle("~r~Suspect~w~: *slurring* What you want, officer pigfucker?");
-                        }
-                        if (counter == 3)
-                        {
-                            suspect.Tasks.PlayAnimation(new AnimationDictionary("amb@world_human_stand_impatient@female@no_sign@base"), "base", -1f, AnimationFlags.Loop);
-                            Game.DisplaySubtitle("~b~You~w~: Ok, we'll do a few tests to see if you're drunk.");
-                        }
-                        if (counter == 4)
-                        {
-                            suspect.Tasks.PlayAnimation(new AnimationDictionary("random@drunk_driver_1"), "drunk_driver_stand_loop_dd2", -1f, AnimationFlags.Loop);
-                            Game.DisplaySubtitle("~r~Suspect~w~: *slurring* You got to catch me first, donut eater.");
-                        }
-                        if (counter == 5)
-                        {
-                            Game.DisplaySubtitle("Convo ended. Chase and arrest the suspect.");
-                            suspect.Tasks.FightAgainst(MainPlayer);
-                            suspect.Armor = 500;
-                            if (suspect != null && suspect.Exists() && suspect.IsValid())
-                                SafeInventory.SafeGiveWeapon(suspect, wepList[new Random().Next((int)wepList.Length)], 500, true);
-                        }
+                        HandleInteraction();
                     }
                     catch (Exception ex)
                     {
@@ -115,7 +105,6 @@ namespace Adam69Callouts.Callouts
                             Settings.EnableLogs = false;
                         }
                     }
-
                 }
             }
 
@@ -155,6 +144,138 @@ namespace Adam69Callouts.Callouts
             base.Process();
         }
 
+        private void HandleInteraction()
+        {
+            switch (scenario)
+            {
+                case DrunkScenario.Aggressive:
+                    HandleAggressiveScenario();
+                    break;
+                case DrunkScenario.Compliant:
+                    HandleCompliantScenario();
+                    break;
+                case DrunkScenario.Flees:
+                    HandleFleesScenario();
+                    break;
+                case DrunkScenario.PassesOut:
+                    HandlePassesOutScenario();
+                    break;
+            }
+        }
+
+        // Scenario 1: Suspect argues and then turns violent
+        private void HandleAggressiveScenario()
+        {
+            switch (counter)
+            {
+                case 1:
+                    NativeFunction.Natives.TASK_TURN_PED_TO_FACE_ENTITY(suspect, MainPlayer, -1);
+                    Game.DisplaySubtitle("~b~You~w~: What goin' on, feller? Have anything to drink today?");
+                    break;
+                case 2:
+                    suspect.Tasks.PlayAnimation(new AnimationDictionary("random@drunk_driver_1"), "drunk_driver_stand_loop_dd2", -1f, AnimationFlags.Loop);
+                    Game.DisplaySubtitle("~r~Suspect~w~: *slurring* What you want, officer pigfucker?");
+                    break;
+                case 3:
+                    suspect.Tasks.PlayAnimation(new AnimationDictionary("amb@world_human_stand_impatient@female@no_sign@base"), "base", -1f, AnimationFlags.Loop);
+                    Game.DisplaySubtitle($"~b~You~w~: {malefemale}, I'm going to need you to calm down.");
+                    break;
+                case 4:
+                    suspect.Tasks.PlayAnimation(new AnimationDictionary("random@drunk_driver_1"), "drunk_driver_stand_loop_dd2", -1f, AnimationFlags.Loop);
+                    Game.DisplaySubtitle("~r~Suspect~w~: *slurring* You got to catch me first, donut eater!");
+                    break;
+                case 5:
+                    Game.DisplaySubtitle("~y~Suspect is hostile! Chase and arrest!");
+                    suspect.Tasks.FightAgainst(MainPlayer);
+                    suspect.Armor = 500;
+                    if (suspect != null && suspect.Exists() && suspect.IsValid())
+                        SafeInventory.SafeGiveWeapon(suspect, wepList[random.Next(wepList.Length)], 500, true);
+                    break;
+            }
+        }
+
+        // Scenario 2: Suspect is too drunk to resist and cooperates
+        private void HandleCompliantScenario()
+        {
+            switch (counter)
+            {
+                case 1:
+                    NativeFunction.Natives.TASK_TURN_PED_TO_FACE_ENTITY(suspect, MainPlayer, -1);
+                    Game.DisplaySubtitle("~b~You~w~: Hey there, have you been drinking today?");
+                    break;
+                case 2:
+                    suspect.Tasks.PlayAnimation(new AnimationDictionary("random@drunk_driver_1"), "drunk_driver_stand_loop_dd2", -1f, AnimationFlags.Loop);
+                    Game.DisplaySubtitle("~r~Suspect~w~: *slurring* ...maaaybe jus' a lil' bit, ossifer...");
+                    break;
+                case 3:
+                    Game.DisplaySubtitle($"~b~You~w~: {malefemale}, I'm going to need you to put your hands behind your back.");
+                    break;
+                case 4:
+                    suspect.Tasks.PlayAnimation(new AnimationDictionary("amb@world_human_stand_impatient@female@no_sign@base"), "base", -1f, AnimationFlags.Loop);
+                    Game.DisplaySubtitle("~r~Suspect~w~: *slurring* Okay, okay... don't hurt me...");
+                    break;
+                case 5:
+                    Game.DisplaySubtitle("~g~Suspect is compliant. Detain and transport.");
+                    LSPD_First_Response.Mod.API.Functions.IsPedArrested(suspect);
+                    break;
+            }
+        }
+
+        // Scenario 3: Suspect panics and flees on contact
+        private void HandleFleesScenario()
+        {
+            switch (counter)
+            {
+                case 1:
+                    NativeFunction.Natives.TASK_TURN_PED_TO_FACE_ENTITY(suspect, MainPlayer, -1);
+                    Game.DisplaySubtitle("~b~You~w~: Hey! Stop right there!");
+                    break;
+                case 2:
+                    suspect.Tasks.PlayAnimation(new AnimationDictionary("random@drunk_driver_1"), "drunk_driver_stand_loop_dd2", -1f, AnimationFlags.Loop);
+                    Game.DisplaySubtitle("~r~Suspect~w~: *slurring* Oh no no no no no...");
+                    GameFiber.Wait(800);
+                    suspect.Tasks.ReactAndFlee(MainPlayer);
+                    blip.Color = System.Drawing.Color.Yellow;
+                    break;
+                case 3:
+                    Game.DisplaySubtitle("~b~You~w~: Stop! Police! Get on the ground!");
+                    suspect.Tasks.ReactAndFlee(MainPlayer);
+                    break;
+                case 4:
+                    Game.DisplaySubtitle("~y~Suspect is fleeing! Pursue and apprehend!");
+                    break;
+            }
+        }
+
+        // Scenario 4: Suspect is too far gone and passes out
+        private void HandlePassesOutScenario()
+        {
+            switch (counter)
+            {
+                case 1:
+                    NativeFunction.Natives.TASK_TURN_PED_TO_FACE_ENTITY(suspect, MainPlayer, -1);
+                    Game.DisplaySubtitle("~b~You~w~: Sir, are you alright? Have you been drinking?");
+                    break;
+                case 2:
+                    suspect.Tasks.PlayAnimation(new AnimationDictionary("random@drunk_driver_1"), "drunk_driver_stand_loop_dd2", -1f, AnimationFlags.Loop);
+                    Game.DisplaySubtitle("~r~Suspect~w~: *incoherent mumbling* ...tequilaaa...");
+                    break;
+                case 3:
+                    Game.DisplaySubtitle("~b~You~w~: Dispatch, I've got a heavily intoxicated individual. Requesting EMS.");
+                    LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("REQUESTING_ASSISTANCE_02");
+                    break;
+                case 4:
+                    suspect.Tasks.PlayAnimation(new AnimationDictionary("missheistdockssetup1ig_14"), "base_idle_a", -1f, AnimationFlags.Loop);
+                    Game.DisplaySubtitle("~r~Suspect~w~: *collapses onto the ground*");
+                    NativeFunction.Natives.SET_PED_TO_RAGDOLL(suspect, 5000, 5000, 0, false, false, false);
+                    break;
+                case 5:
+                    Game.DisplaySubtitle("~g~Suspect is down. Secure the scene and wait for EMS.");
+                    LSPD_First_Response.Mod.API.Functions.IsPedArrested(suspect);
+                    break;
+            }
+        }
+
         public override void End()
         {
             if (suspect != null && suspect.Exists()) suspect.Dismiss();
@@ -165,7 +286,6 @@ namespace Adam69Callouts.Callouts
             if (Settings.MissionMessages)
             {
                 BigMessageThread bigMessage = new BigMessageThread();
-
                 bigMessage.MessageInstance.ShowColoredShard("~g~Code 4", "Suspect Neutralized!", RAGENativeUI.HudColor.Green, RAGENativeUI.HudColor.Black, 5000);
             }
             else
@@ -178,7 +298,6 @@ namespace Adam69Callouts.Callouts
 
             if (Settings.EnableLogs)
             {
-
                 Game.LogTrivial("Adam69 Callouts [LOG]: Deranged Drunken Feller callout is CODE 4!");
                 LoggingManager.Log("Adam69 Callouts [LOG]: Deranged Drunken Feller callout is CODE 4!");
             }
